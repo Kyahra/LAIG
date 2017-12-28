abd09dc7bdb5744543a42a1afda6dcc34282b6c5
dolfander <daniel.pmachado@hotmail.com>
2017-12-28 16:18:12 +0000

    prolog requests

--- PraticalWork3/prolog/src/utils.pl
+++ PraticalWork3/prolog/src/utils.pl
@@ -65,9 +65,25 @@ is_set(Lst) :-
     setof(X, member(X, Lst), Set),
     length(Lst, N),
     length(Set, N).
-	
-	
+
+
 initialize(Board):-
 	board4(B),
     createBoard(B,Board).
 
+% humanPlay(ValidMove, T, [6,0],[7,1],[0,1],[0,2],[ivory,green,red,blue,black], NewBoard, NewPlayer, NewColors)
+% do not use NewBoard when ValidMove=0
+humanPlay(ValidMove,Board,PosInit,PosFinal,P1,P2,Colors,NewBoard,NewPlayer,NewColors):-
+       nth0(0,PosInit, X1),
+       nth0(1,PosInit, Y1),
+       nth0(0,PosFinal, X2),
+       nth0(1,PosFinal, Y2),
+       checkValidMove(ValidMove,Board,P1,P2,X1,Y1,X2,Y2),
+       (ValidMove = 1 ->  setPosition(Board, B, 0, 0, X1, Y1, [x]),
+                         index(Board,Y1,X1,InitPos),
+                         index(Board,Y2,X2,FinalPos),
+                         append(InitPos,FinalPos,Final),
+                         updatePlayer(B,P1,Final,X2,Y2,NewBoard,NewPlayer),
+						  NewColors =Colors;
+                         write(' || Invalid Move ||')
+                      ).
--- PraticalWork3/reader/MyPrologInterface.js
+++ PraticalWork3/reader/MyPrologInterface.js
@@ -15,3 +15,20 @@ function prologRequestError(data) {
     console.log('Prolog request error:');
     console.log(data);
 }
+
+
+function humanPlay(validMove, board, initPos, finalPos, player1, player2, colors, newBoard, newPlayer, newColors, callback) {
+    let requestString = 'humanPlay('
+        + validMove + ','
+        + JSON.stringify(board) + ','
+        + JSON.stringify(initPos) + ','
+        + JSON.stringify(finalPos) + ','
+        + JSON.stringify(player1) + ','
+        + JSON.stringify(player2) + ','
+        + JSON.stringify(colors) + ','
+        + JSON.stringify(newBoard) + ','
+        + JSON.stringify(newPlayer) + ','
+        + JSON.stringify(newColors) + ')';
+
+    getPrologRequest(requestString, callback);
+}
